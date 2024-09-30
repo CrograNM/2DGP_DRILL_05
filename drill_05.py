d193d5c98b79fe79de9c11fa98d364c3857ff6ce
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2024-09-30 18:51:24 +0900

    스프라이트 시트 편집

--- drill_05.py
+++ drill_05.py
@@ -3,8 +3,7 @@ from pico2d import *
 
 open_canvas()
 ground = load_image('TUK_GROUND.png')
-character_run = load_image('RunSheet.png')
-character_idle = load_image('IdleSheet.png')
+character = load_image('SpriteSheet.png')
 
 def handle_events():
     global playing, running
@@ -19,7 +18,8 @@ def handle_events():
 playing = True
 running = True
 x = 800 // 2
-frame = 0
+frame_run = 0
+frame_idle = 0
 dir = 0
 
 # fill here
@@ -28,9 +28,9 @@ while playing:
         ground.draw(400, 300, 800, 600)
 
         if running:
-            character_run.clip_draw(frame * 32, 0, 32, 32, x, 90, 60, 60)
+            character.clip_draw(frame * 32, 0, 32, 32, x, 90, 60, 60)
         else:
-            character_idle.clip_draw(frame * 32, 0, 32, 32, x, 90, 60, 60)
+            character.clip_draw(frame * 32, 0, 32, 32, x, 90, 60, 60)
 
         update_canvas()
         handle_events()
