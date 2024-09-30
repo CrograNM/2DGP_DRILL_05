02b5d3e99031f58121a99a07161bd6b5c6e6766f
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2024-09-30 19:20:23 +0900

    달릴때 좌우 전환

--- drill_05.py
+++ drill_05.py
@@ -6,16 +6,18 @@ ground = load_image('TUK_GROUND.png')
 character = load_image('sprite_sheet.png')
 
 def handle_events():
-    global playing, running, Xdir, Ydir
+    global playing, running, Xdir, Ydir, isLeft
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             playing = False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
+                isLeft = False
                 running = True
                 Xdir += 1
             elif event.key == SDLK_LEFT:
+                isLeft = True
                 running = True
                 Xdir -= 1
             if event.key == SDLK_UP:
@@ -44,12 +46,14 @@ def handle_events():
 
 playing = True
 running = False
+isLeft = False
 x = 800 // 2
 y = 170
-frame_run = 0
-frame_idle = 0
 Xdir = 0
 Ydir = 0
+frame_run = 0
+frame_idle = 0
+
 
 # fill here
 while playing:
@@ -57,13 +61,18 @@ while playing:
         ground.draw(400, 300, 800, 600)
 
         if running:
-            #character.clip_draw(frame_run * 50, 0, 50, 80, x, y, 50, 80)
-            character.clip_composite_draw(frame_run * 50, 0, 50, 80, 0, 'h', x, y, 50, 80)
+            if isLeft:
+                character.clip_composite_draw(frame_run * 50, 0, 50, 80, 0, 'h', x, y, 50, 80)
+            else:
+                character.clip_draw(frame_run * 50, 0, 50, 80, x, y, 50, 80)
             frame_run = (frame_run + 1) % 4
         else:
-            #character.clip_draw(frame_idle * 38, 80, 38, 80, x, y, 38, 80)
-            character.clip_composite_draw(frame_idle * 38, 80, 38, 80, 0, 'h', x, y, 38, 80)
+            if isLeft:
+                character.clip_composite_draw(frame_idle * 38, 80, 38, 80, 0, 'h', x, y, 38, 80)
+            else:
+                character.clip_draw(frame_idle * 38, 80, 38, 80, x, y, 38, 80)
             frame_idle = (frame_idle + 1) % 6
+
         x += Xdir * 7
         y += Ydir * 5
         update_canvas()
