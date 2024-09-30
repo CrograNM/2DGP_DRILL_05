018c4d613a7e9dec6753e27de8c42b8b4f527772
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2024-09-30 16:58:46 +0900

    반복문 조건 수정

--- drill_05.py
+++ drill_05.py
@@ -7,30 +7,35 @@ character_run = load_image('RunSheet.png')
 character_idle = load_image('IdleSheet.png')
 
 def handle_events():
-    global running
-    
+    global playing, running
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
-            running = False
+            playing = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
+            playing = False
     pass
 
+playing = True
 running = True
 x = 800 // 2
 frame = 0
 dir = 0
 
 # fill here
-while running:
-    clear_canvas()
-    ground.draw(400, 300, 800, 600)
-    character_idle.clip_draw(frame*32, 0, 32, 32, x, 90, 90, 90)
-    update_canvas()
-    handle_events()
-    frame = (frame + 1) % 8
-    delay(0.05)
+while playing:
+        clear_canvas()
+        ground.draw(400, 300, 800, 600)
+
+        if running:
+            character_run.clip_draw(frame * 32, 0, 32, 32, x, 90, 60, 60)
+        else:
+            character_idle.clip_draw(frame * 32, 0, 32, 32, x, 90, 60, 60)
+
+        update_canvas()
+        handle_events()
+        frame = (frame + 1) % 8
+        delay(0.05)
 
 close_canvas()
 
