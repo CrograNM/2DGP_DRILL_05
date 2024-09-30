a393dd73fe63710f858eba2f1bdd55b269284fe5
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2024-09-30 19:15:10 +0900

    y축 이동 및 idle, run 애니메이션 구분

--- drill_05.py
+++ drill_05.py
@@ -6,26 +6,40 @@ ground = load_image('TUK_GROUND.png')
 character = load_image('sprite_sheet.png')
 
 def handle_events():
-    global playing, running, dir
+    global playing, running, Xdir, Ydir
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             playing = False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
-                dir += 1
+                running = True
+                Xdir += 1
             elif event.key == SDLK_LEFT:
-                dir -= 1
+                running = True
+                Xdir -= 1
+            if event.key == SDLK_UP:
+                running = True
+                Ydir += 1
+            elif event.key == SDLK_DOWN:
+                running = True
+                Ydir -= 1
             elif event.key == SDLK_ESCAPE:
                 playing = False
 
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
                 running = False
-                dir -= 1
+                Xdir -= 1
             elif event.key == SDLK_LEFT:
                 running = False
-                dir += 1
+                Xdir += 1
+            if event.key == SDLK_UP:
+                running = False
+                Ydir -= 1
+            elif event.key == SDLK_DOWN:
+                running = False
+                Ydir += 1
     pass
 
 playing = True
@@ -34,7 +48,8 @@ x = 800 // 2
 y = 170
 frame_run = 0
 frame_idle = 0
-dir = 0
+Xdir = 0
+Ydir = 0
 
 # fill here
 while playing:
@@ -42,15 +57,18 @@ while playing:
         ground.draw(400, 300, 800, 600)
 
         if running:
-            character.clip_draw(frame_run * 50, 0, 50, 80, x, y, 50, 80)
+            #character.clip_draw(frame_run * 50, 0, 50, 80, x, y, 50, 80)
+            character.clip_composite_draw(frame_run * 50, 0, 50, 80, 0, 'h', x, y, 50, 80)
             frame_run = (frame_run + 1) % 4
         else:
-            character.clip_draw(frame_idle * 38, 80, 38, 80, x, y, 38, 80)
+            #character.clip_draw(frame_idle * 38, 80, 38, 80, x, y, 38, 80)
+            character.clip_composite_draw(frame_idle * 38, 80, 38, 80, 0, 'h', x, y, 38, 80)
             frame_idle = (frame_idle + 1) % 6
-        x += dir * 5
+        x += Xdir * 7
+        y += Ydir * 5
         update_canvas()
         handle_events()
-        delay(0.1)
+        delay(0.08)
 
 close_canvas()
 
